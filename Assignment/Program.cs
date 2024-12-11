400609213f8aaed4d381487a7a0228f9bcecdd8f
Ahmed El-Kholy <a.adel27111@gmail.com>
2024-12-11 19:49:44 +0200

    Add project files.

---
+++ Assignment/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Q1 Write a program that takes a number from the user then print yes if that number can be divided by 3 and 4 otherwise print no.
+            //int.TryParse(Console.ReadLine(), out var num);
+            //if ((num % 3) == 0 && (num % 4) == 0)
+            //    Console.WriteLine("Yes");
+            //else
+            //    Console.WriteLine("No");
+            #endregion
+
+
+        }
+    }
+}
---
+++ Demo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int.TryParse(Console.ReadLine(), out int Age);
+            switch (Age)
+            {
+                case 22:
+                    Console.WriteLine("Your Age is Greater than 22");
+                    break;
+                case 23:
+                    Console.WriteLine("Your Age is Less than 22");
+                    break;
+                default:
+                    Console.WriteLine("Your Age equals 22");
+                    break;
+            }
+        }
+    }
+}
