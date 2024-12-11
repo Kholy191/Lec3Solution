2a00160166d318e7468f6b1a6bea49a32dabbedc
Ahmed El-Kholy <a.adel27111@gmail.com>
2024-12-11 19:52:38 +0200

    Q1

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -5,19 +5,8 @@ namespace Demo
     {
         static void Main(string[] args)
         {
-            int.TryParse(Console.ReadLine(), out int Age);
-            switch (Age)
-            {
-                case 22:
-                    Console.WriteLine("Your Age is Greater than 22");
-                    break;
-                case 23:
-                    Console.WriteLine("Your Age is Less than 22");
-                    break;
-                default:
-                    Console.WriteLine("Your Age equals 22");
-                    break;
-            }
+
         }
     }
 }
+
